0f1461ee2e4ac52ef9aee544b5fae85e6732a061
Farouk-Ben-Amor <faaroukbnamor@gmail.com>
2023-05-03 01:19:13 +0100

    done profiles+cv template+install pdfkit/wkhtmltox

--- pdf/urls.py
+++ pdf/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.accept , name = "accept"),
+    path('<int:id>/', views.resume , name = "resume"),
+    path('list/', views.list , name = "list"),
 ]
